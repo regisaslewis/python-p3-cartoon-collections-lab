bdae8457835f1a9c39435c9c965ef3c90d274618
RegisLewis <regisaslewis@gmail.com>
2023-06-04 00:21:31 -0700

    cartoon collections lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for n in names:
+        print(f"{names.index(n) + 1}. {n}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    list = [n.capitalize() + "!" for n in planeteer_calls]
+    return list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for n in calls:
+        if len(n) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    if "cheddar" in list:
+        return "cheddar"
+    elif "gouda" in list:
+        return "gouda"
+    elif "camembert" in list:
+        return "camembert"
+    else:
+        return None
